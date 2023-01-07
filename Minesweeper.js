65215aa2f6cc8564a7d0bfb2cd66ef49da103d23
MihaiAndreiConstantin <117904698+MihaiAndreiConstantin@users.noreply.github.com>
2023-01-07 20:35:23 +0200

    Update Minesweeper.js

--- Minesweeper.js
+++ Minesweeper.js
@@ -1,24 +1,38 @@
-let level
-let columns = 0, line = 0
+let level = 0
+let columns = 0
+let lines = 0
+let gameboardSize = 0
+let levelSelectDiv = document.getElementById("level-select")
 
 function levelSelect(x) {
     if (x == 1) {
         columns = 10
-        lines = 8
+        lines = 10
     } else if (x == 2) {
-        columns = 18
-        lines = 14
+        columns = 15
+        lines = 15
+        
     } else {
-        columns = 24
+        columns = 20
         lines = 20
     }
     createGameboard()
+    setGameboardSize()
 }
 
 function createGameboard() {
-    for (let i = 1; i <= lines; ++i) {
-        for (let j = 1; j <= columns; ++j) {
-            document.getElementById("gameboard").innerHTML = '<div class="fields">-</div>'
-        }
-    } 
+    preparePlayTable()
+    for (let i = 1; i <= lines * columns; ++i) {
+        document.getElementById("gameboard").innerHTML += '<button type="button" class="fields" id="i">💣</button>'     
+    }
+}
+
+function preparePlayTable() {
+    levelSelectDiv.remove()
+}
+
+function setGameboardSize() {
+    gameboardSize = (lines * 10) + (columns * 10) + 4
+    document.getElementById("gameboard").style.width = gameboardSize + "px";
+    document.getElementById("gameboard").style.height = gameboardSize + "px";
 }
